2f3b934857be3fe4aa676adabc0e8511bbc516cd
AndhikaNayakaAW <andhikanayaka@gmail.com>
2024-09-11 11:55:41 +0700

    update mental health tracker

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+import uuid  # add this line at the very top
+
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # add this line
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
@@ -9,3 +12,5 @@ class MoodEntry(models.Model):
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
+    
+    
